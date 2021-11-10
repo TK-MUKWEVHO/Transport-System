86942533b4e926f15db247be8397d2f85c1f49c1
TK-MUKWEVHO <82998109+TK-MUKWEVHO@users.noreply.github.com>
2021-11-10 13:41:30 +0200

    save

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,16 @@ import StudentTrip from "./components/StudentTrip/studentTrip";
 import useStyle from './styles';
 import {useDispatch} from 'react-redux';
 import {getTrip} from './actions/studentTrip';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/material/Menu';
+import AppBar from '@mui/material/AppBar';
+import SignIn from "./components/signin/SignIn";
 
 const App=()=>{
     const Classes = useStyle();
@@ -12,12 +22,30 @@ const App=()=>{
         dispatch(getTrip())
     }, [dispatch])
     return(
-        <div>
-            <h1>Hellow World</h1>
-            <Form/>
-            <StudentTrip/>
-        </div>
         
+        <Container maxWith="sm">
+            <Box>
+            <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" align="center" component="div" sx={{ flexGrow: 1 }}>
+            TUT BUS SERVICE
+          </Typography>
+          
+        </Toolbar>
+      </AppBar>
+
+        <SignIn/>
+    </Box>
+    </Container>
     );
 }
 
